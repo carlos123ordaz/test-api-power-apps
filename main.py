e65152f841febc1f5ae9ea70b916af5e76ad5829
ORDAZ <cordaz@corsusa.com>
2026-01-06 15:41:21 -0500

    archivos

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import base64
 
 app = FastAPI(title="API de Saludo", version="1.0.0")
 
 
-@app.get("/")
-def root():
-    return {"mensaje": "¡Bienvenido a la API de Saludo!"}
+class Attachment(BaseModel):
+    fileName: str
+    contentType: str
+    fileBase64: str
+    emailFrom: str | None = None
+    subject: str | None = None
 
 
 @app.post("/saludar")
-def saludar_post():
-    print("Saludo recibido")
+def saludar_post(data: Attachment):
+    file_bytes = base64.b64decode(data.fileBase64)
+    with open(data.fileName, "wb") as f:
+        f.write(file_bytes)
+
+    print(f"Archivo recibido: {data.fileName}")
+    print(f"Correo de: {data.emailFrom}")
+
     return {
-        "mensaje": f"Recibimos tu saludo",
+        "mensaje": "Archivo recibido correctamente",
+        "archivo": data.fileName
     }
